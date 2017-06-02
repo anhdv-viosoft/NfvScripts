e2f3be729c4e092b3edd7399abe0c3d817d600c3
anhdv-viosoft <anhdv@viosoft.com>
2017-06-02 14:54:28 +0700

    Update 03-Network-setting.sh

--- setup/03-Network-setting.sh
+++ setup/03-Network-setting.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-# Disable network manager
-sudo systemctl disable NetworkManager.service
-sudo systemctl disable firewalld.service
-sudo chkconfig network on
-#sudo systemctl restart network.service
-sudo systemctl stop NetworkManager.service
+YUM_CMD=$(which yum)
+APT_GET_CMD=$(which apt-get)
+  
+if [[ ! -z $YUM_CMD ]]; then
+  # Disable network manager
+  sudo systemctl disable NetworkManager.service
+  sudo systemctl disable firewalld.service
+  sudo chkconfig network on
+  #sudo systemctl restart network.service
+  sudo systemctl stop NetworkManager.service
+elif [[ ! -z $APT_GET_CMD ]]; then
+  # Disable Firewall
+  ufw disable
+  service ufw stop
+fi
