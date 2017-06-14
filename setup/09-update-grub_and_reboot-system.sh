18e1f77d7dffd5e900cefd59e6bda7cbf020ec2f
anhdv-viosoft <anhdv@viosoft.com>
2017-06-14 14:38:18 +0700

    Create 09-update-grub_and_reboot-system.sh

--- setup/09-update-grub_and_reboot-system.sh
+++ setup/09-update-grub_and_reboot-system.sh
@@ -13,7 +13,7 @@ echo "The following logical cores are setup for NICs usage:"
 echo "====================================================="
 echo $cores
 echo "====================================================="
-
+sed -i -e "/^GRUB_CMDLINE_LINUX/c\GRUB_CMDLINE_LINUX=\"\"" /etc/default/grub
 sed -i -e "/^GRUB_CMDLINE_LINUX/ s/\"\$/ quiet default_hugepagesz=$HUGEPAGESZ hugepagesz=$HUGEPAGESZ hugepages=$HUGEPAGES\"/" /etc/default/grub
 sed -i -e "/^GRUB_CMDLINE_LINUX/ s/\"\$/ isolcpus=$cores\"/" /etc/default/grub
 sed -i -e "/^GRUB_CMDLINE_LINUX/ s/\"\$/ console=ttyS1,115200n8 noirqbalance\"/" /etc/default/grub
